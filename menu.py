5be821dcccf8d1cec706fd053d5a9ef4a6dcb24d
Brendan <pidouxbrendan@gmail.com>
2024-12-01 18:14:28 +0100

    Menu principale

---
+++ menu.py
@@ -0,0 +1,21 @@
+# Menu Principal
+choix = ''
+while choix != 'q':
+    # Affichage des choix
+    print('\n==== Menu principal ====')
+    print('1: Lister les todos')
+    print('2: Créer un todo')
+    print('3: Changer le statut d\'un todo')
+    print('4: Supprimer un todo')
+    print('q: Quitter')
+    print('========================')
+
+    # Actions suivant le choix
+    choix = input('=> Choix : ').strip()
+    match choix:
+        case '1': lister_todos()
+        case '2': creer_todo()
+        case '3': modifier_statut_todo()
+        case '4': supprimer_todo()
+        case 'q': print('Au revoir!')
+        case _: print('Choix invalide, réessayez.')
